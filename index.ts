94cb56be87294185443eccd60b37d76356776dbd
nurodev <me@nuro.dev>
2021-02-23 02:33:44 +0000

    Added WIP Vue 3.x plugin
    Pivoted away from Vite specific plugin and instead attempting to make the module more targetted for Vue 3.x usage

--- index.ts
+++ index.ts
@@ -1,59 +1,74 @@
-import type { Plugin } from 'vite';
 import type { Metric } from 'web-vitals';
+import type { Router } from 'vue-router';
 
-export interface VercelOptions {
+export interface VitalsOptions {
 	/**
 	 * Prints metrics in the console (Optional)
+	 * 
 	 * @default false
 	 */
 	debug?: boolean;
+	/**
+	 * Pass the app router instance to vitals to catch route changes (Required)
+	 */
+	router: Router;
+	/**
+	 * URL endpoint to upload vital metric data (Optional)
+	 * 
+	 * @default 'https://vitals.vercel-analytics.com/v1/vitals'
+	 */
+	url?: string;
 }
 
-interface SendOptions {
+interface MetricOptions {
 	/**
-	 * Whether to log metric details to the console (Optional)
+	 * Prints metrics in the console (Optional)
+	 * 
 	 * @default false
 	 */
-	debug?: boolean;
+	debug: boolean;
 	/**
-	 * Full path to the page route for metric being sent
+	 * Current full route path to be sent (Required)
 	 */
 	full_path: string;
 	/**
-	 * The metric data being sent to Vercel
+	 * The metric data being sent
 	 */
 	metric: Metric;
+	/**
+	 * URL endpoint to upload vital metric data (Optional)
+	 * 
+	 * @default 'https://vitals.vercel-analytics.com/v1/vitals'
+	 */
+	url: string;
 }
 
-const VITALS_URL = 'https://vitals.vercel-analytics.com/v1/vitals';
-
-const send_to_analytics = (send_options: SendOptions): void => {
-	const {
-		debug = false,
-		full_path,
-		metric,
-	} = send_options;
-
+/**
+ * Send a single performance metric to the provided analytics endpoint
+ * 
+ * @param {MetricOptions} options
+ */
+const send_to_analytics = (options: MetricOptions): void => {
 	const body = {
-		dsn: process.env.VERCEL_ANALYTICS_ID,
-		id: metric.id,
-		page: full_path,
+		dsn: process.env.VERCEL_ANALYTICS_ID, // TODO: Add as customizable parameter
+		id: options.metric.id,
+		page: options.full_path,
 		href: location.href,
-		event_name: metric.name,
-		value: metric.value.toString(),
+		event_name: options.metric.name,
+		value: options.metric.value.toString(),
 		speed: 'connection' in navigator && navigator['connection'] && 'effectiveType' in navigator['connection'] ? navigator['connection']['effectiveType'] : '',
 	};
 
-	if (debug) console.log(metric.name, JSON.stringify(body, null, 2));
+	if (options.debug) console.log(options.metric.name, JSON.stringify(body, null, 2));
 
 	const blob = new Blob([
-		new URLSearchParams(body).toString(),
+		new URLSearchParams(JSON.stringify(body)).toString(),
 	], {
 		// This content type is necessary for `sendBeacon`:
 		type: 'application/x-www-form-urlencoded',
 	});
 
-	(navigator.sendBeacon && navigator.sendBeacon(VITALS_URL, blob)) || fetch(VITALS_URL, {
+	(navigator.sendBeacon && navigator.sendBeacon(options.url, blob)) || fetch(options.url, {
 		body: blob,
 		method: 'POST',
 		credentials: 'omit',
@@ -61,19 +76,17 @@ const send_to_analytics = (send_options: SendOptions): void => {
 	});
 }
 
-export const Vercel = (options: VercelOptions): Plugin => {
+export const useVitals = async (options: VitalsOptions): Promise<void> => {
 	const {
 		debug = false,
+		router,
+		url = 'https://vitals.vercel-analytics.com/v1/vitals',
 	} = options;
 
-	return {
-		name: 'vite-plugin-vercel',
-		enforce: 'post',
-		apply: 'build',
-		async load(id) {
+	try {
+		await router.isReady();
+		router.beforeEach(async to => {
 			try {
-				const full_path = '';
-
 				const {
 					getCLS,
 					getFCP,
@@ -82,16 +95,23 @@ export const Vercel = (options: VercelOptions): Plugin => {
 					getTTFB,
 				} = await import('web-vitals');
 
-				getCLS(metric => send_to_analytics({ debug, full_path, metric }));
-				getFCP(metric => send_to_analytics({ debug, full_path, metric }));
-				getFID(metric => send_to_analytics({ debug, full_path, metric }));
-				getLCP(metric => send_to_analytics({ debug, full_path, metric }));
-				getTTFB(metric => send_to_analytics({ debug, full_path, metric }));
+				const metric_options = (metric: Metric): MetricOptions => ({
+					debug,
+					full_path: to.fullPath,
+					metric,
+					url,
+				});
+
+				getCLS(metric => send_to_analytics(metric_options(metric)));
+				getFCP(metric => send_to_analytics(metric_options(metric)));
+				getFID(metric => send_to_analytics(metric_options(metric)));
+				getLCP(metric => send_to_analytics(metric_options(metric)));
+				getTTFB(metric => send_to_analytics(metric_options(metric)));
 			} catch (err) {
-				console.error('[vite-plugin-vercel]', err);
+				console.error('[vue-web-vitals]', err);
 			}
-		},
+		});
+	} catch (error) {
+		console.error(`[vue-web-vitals]`, error);
 	}
 };
-
-export default Vercel;
